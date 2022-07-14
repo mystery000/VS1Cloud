607251454366c11cbda6c726ad21622ce5241067
Fatih Gucuko <fatih@zuusoft.com>
2022-07-14 16:29:48 +0200

    adding currency crons update and save feature

---
+++ server/Currency/FxApi.js
@@ -0,0 +1,103 @@
+import { Meteor } from "meteor/meteor";
+
+class FxApi {
+  static ApiID = "webinfy166640657";
+  static ApiKey = "peorsc7t96poaf2k8t1segj0u2";
+  static encodedApiKey =
+    "d2ViaW55Zmk2MzU1NjQ4MDg6czluZnFia3BjaTBpMzNrMnZybDJwOTZ2dm4=";
+
+  constructor() {}
+
+  /**
+   *
+   * @param {String} to
+   * @param {String} from
+   * @param {float} amount
+   * @returns {Promise<{buy: float, sell: float}>}
+   */
+  async getExchangeRate(to = "EUR", from = "AUD", amount = 1, res) {
+  
+      
+      Meteor.http.call(
+        "GET",
+        `https://xecdapi.xe.com/v1/convert_to.json/?to=${to}&from=${from}&amount=${amount}&inverse=true`,
+        {
+          // data: postData,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Basic " + FxApi.encodedApiKey,
+          },
+        },
+        (error, response) => {
+          if (error) {
+          } else {
+            console.log('Fetched repsoons: ', response);
+            console.log('Final repsonse');
+            if (response.status >= 200 && response.status <= 302) {
+                let data = response.json();
+        
+                const buyRate = data.from[0].mid;
+                const sellRate = data.from[0].inverse;
+        
+               res({
+                  buy: buyRate,
+                  sell: sellRate,
+                });
+              } else {
+                res({
+                  buy: 1.21,
+                  sell: 1.19,
+                });
+              }
+
+          }
+        }
+      );
+  }
+
+  /**
+   * This function should return the buy rate
+   */
+  async getBuyRate(to = "EUR", from = "AUD") {
+    const response = await fetch(
+      `https://xecdapi.xe.com/v1/convert_to.json/?to=${to}&from=${from}&amount=1&inverse=false`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + FxApi.encodedApiKey,
+        },
+      }
+    );
+
+    if (response.status >= 200 && response.status <= 302) {
+      let data = await response.json();
+      const rate = data.from[0].mid;
+      return rate;
+    }
+  }
+
+  async getSellRate(to = "EUR", from = "AUD") {
+    const response = await fetch(
+      `https://xecdapi.xe.com/v1/convert_to.json/?to=${to}&from=${from}&amount=1&inverse=true`,
+      {
+        // Authorization: `${FxApi.ApiID}:${FxApi.ApiKey}`,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + FxApi.encodedApiKey,
+        },
+      }
+    );
+
+    if (response.status >= 200 && response.status <= 302) {
+      let data = await response.json();
+
+      const rate = data.from[0].inverse;
+
+      return rate;
+    }
+  }
+}
+
+export default FxApi = new FxApi();
--- server/CurrencyCron.js
+++ server/CurrencyCron.js
@@ -1,15 +1,24 @@
-import { Meteor } from 'meteor/meteor'
-FutureTasks = new Meteor.Collection('cron-jobs');
+import { Meteor } from "meteor/meteor";
+import CronSetting from "./Currency/CronSetting";
+import FxApi from "./Currency/FxApi";
+FutureTasks = new Meteor.Collection("cron-jobs");
 
+async function asyncForEach(array, callback, onFinished) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+
+  await onFinished();
+}
 
 Meteor.startup(() => {
   const currentDate = new Date();
 
-  FutureTasks.find().forEach(function(setting) {
+  FutureTasks.find().forEach(function (setting) {
     if (setting.startAt < currentDate) {
-      Meteor.call('addCurrencyCron', setting);
+      Meteor.call("addCurrencyCron", setting);
     } else {
-      Meteor.call('scheduleCron', setting);
+      Meteor.call("scheduleCron", setting);
     }
   });
   SyncedCron.start();
@@ -18,7 +27,6 @@ Meteor.startup(() => {
    * The future stasks
    */
   let futureCrons = [];
-
 
   /**
    * Step 2 : We need to check if their date is reached
@@ -32,25 +40,123 @@ Meteor.startup(() => {
   SyncedCron.start();
 });
 
+async function updateCurrencies() {}
+
+async function updateCurrency(currency) {}
+
 Meteor.methods({
+  /**
+   * Here we load only currencies
+   * @param {CronSetting} cronSetting 
+   * @param {Object} erpGet 
+   * @param {CallableFunction} callback 
+   */
+  getCurrencies: async (cronSetting, erpGet, callback) => {
+    console.log("Running cron job for user: " + cronSetting.employeeId);
+
+    let apiUrl =
+      "https://" +
+      erpGet.ERPIPAddress +
+      ":" +
+      erpGet.ERPPort +
+      "/erpapi/TCurrency?ListType=Detail";
+    console.log(apiUrl);
+
+    /* My only fear is how do you pass the header details to this form? */
+    /* Not diffuclt if you pass it from client to this place */
+    let postHeaders = {
+      database: erpGet.ERPDatabase,
+      username: erpGet.ERPUsername,
+      password: erpGet.ERPPassword,
+      // "Access-Control-Allow-Origin": "*"
+    };
+
+    /**
+     * Here we GET all tCurrency of the currency user
+     */
+    await Meteor.http.call("GET", apiUrl, { headers: postHeaders }, (error, result) => {
+      if(error) {
+        console.log("error");
+      } else {
+        console.log("result");
+        // console.log(result);
+      }
+    });
+  },
+
+  /**
+   * 
+   * @param {Object} currencies 
+   * @param {CallableFunction} onFinishedCallback 
+   */
+  updateCurrencies: async (currencies, onFinishedCallback) => {
+    /**
+     * First we update the list of currencies
+     */
+    await asyncForEach(currencies, async (currency, index) => {
+      console.log("currency", index, currency.fields.Code);
+      //currencies[index] = await Meteor.call("updateCurrency", currency);
+    });
+    await onFinishedCallback(currencies);
+  },
   updateCurrency: async (currency) => {
+    console.log('updating currency', currency.fields.Code);
     /**
      * We need to make an API call to get the object
      */
-    
+    const response = await FxApi.getExchangeRate(
+      currency.fields.Code,
+      "AUD",
+      1
+    );
 
-    currency.BuyRate = "";
-    currency.sellRate = "";
+    currency.fields.BuyRate = response.buy;
+    currency.fields.SellRate = response.sell;
 
     return currency;
 
+    // return currency;
+  },
+  saveCurrencies: async (currencies = [], erpGet) => {
+    let apiUrl =
+      "https://" +
+      erpGet.ERPIPAddress +
+      ":" +
+      erpGet.ERPPort +
+      "/erpapi/TCurrency?ListType=Detail";
+
+    let postHeaders = {
+      database: erpGet.ERPDatabase,
+      username: erpGet.ERPUsername,
+      password: erpGet.ERPPassword,
+      // "Access-Control-Allow-Origin": "*"
+    };
+
+    console.log("saving currency: ", currencies.length);
+
+    /**
+     * Here we will save ht big object list
+     */
+    Meteor.http.call(
+      "POST",
+      apiUrl,
+      {
+        data: currencies,
+        headers: postHeaders,
+      },
+      (error, result) => {
+        if (error) {
+        } else {
+          console.log(result);
+        }
+      }
+    );
   },
   /**
    * This functions is going to run when the cron is running
    * @param {*} cronSetting
    */
-   runCron: async (cronSetting, erpGet) => {
-    console.log("Running cron job for user: " + cronSetting.employeeId);
+  runCron: async (cronSetting, erpGet) => {
     // await fetch("/cron/currency-update/" + cronSetting.employeeId);
     // We are testing with get request only but we need post request here
     // let app_url="https://sandboxdb.vs1cloud.com:4434/erpapi/TCurrency?ListType=Detail";
@@ -60,54 +166,19 @@ Meteor.methods({
     //     "Accept":"application/json"
     //   },
     // });
-    let apiUrl = 'https://' + erpGet.ERPIPAddress + ':' + erpGet.ERPPort + '/erpapi/TCurrency?ListType=Detail';
-    console.log(apiUrl);
-    console.log("Running cron job for user: " + cronSetting.employeeId);
-    /* My only fear is how do you pass the header details to this form? */
-    /* Not diffuclt if you pass it from client to this place */
-    let postHeaders = {
-      "database": erpGet.ERPDatabase,
-      "username": erpGet.ERPUsername,
-      "password": erpGet.ERPPassword
-      // "Access-Control-Allow-Origin": "*"
-    };
-
-    /**
-     * Here we GET all tCurrency of the currency user
-     */
-    Meteor.http.call("GET", apiUrl, {
-        // data: postData,
-        headers: postHeaders,
-    }, (error, result) => {
-        if (error) {
-
-        } else {
-            console.log(result);
-        }
-    });
 
     /**
      * now we need to loop and update each objects
      */
-
-
-
-
-    /**
-     * Here we will save ht big object list
-     */
-    Meteor.http.call("POST", apiUrl, {
-      // data: postData,
-      headers: postHeaders,
-      body: {},
-  }, (error, result) => {
+    Meteor.call("getCurrencies", cronSetting, erpGet, (error, result) => {
       if (error) {
-
+        console.log(error);
       } else {
-          console.log(result);
+        console.log(result);
+        console.log("get currencies success");
+        Meteor.call('updateCurrencies', result.data.tcurrency)
       }
-  });
-
+    });
   },
   /**
    * This function will just add the cron job
@@ -115,7 +186,7 @@ Meteor.methods({
    * @param {Object} cronSetting
    * @returns
    */
-  addCurrencyCron: (cronSetting,erpGet) => {
+  addCurrencyCron: (cronSetting, erpGet) => {
     const cronId = `currency-update-cron_${cronSetting.id}_${cronSetting.employeeId}`;
     SyncedCron.remove(cronId);
 
@@ -123,12 +194,12 @@ Meteor.methods({
       name: cronId,
       schedule: function (parser) {
         const parsed = parser.text(cronSetting.toParse);
-        return parser.text('every 2 minutes');;
+        return parser.text("every 2 minutes");
       },
       job: () => {
-        console.log(cronSetting.employeeId);
-        Meteor.call("runCron", cronSetting, erpGet,  function(error, results) {
-          console.log(results, error); //results.data should be a JSON object
+        // console.log(cronSetting.employeeId);
+        Meteor.call("runCron", cronSetting, erpGet, function (error, results) {
+          // console.log(results, error); //results.data should be a JSON object
         });
       },
     });
@@ -140,5 +211,5 @@ Meteor.methods({
    */
   scheduleCron: (cronSetting) => {
     FutureTasks.insert(cronSetting);
-  }
+  },
 });
