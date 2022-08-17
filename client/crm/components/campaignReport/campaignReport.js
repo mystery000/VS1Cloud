5d5a670c70b3cdc2215605016a0e57d227bff85c
bluedev088 <bluestars088@gmail.com>
2022-08-17 18:54:58 +0800

    campaign-indexdb

--- client/crm/components/campaignReport/campaignReport.js
+++ client/crm/components/campaignReport/campaignReport.js
@@ -1,7 +1,28 @@
 
 Template.mailchimpCampaignList.onRendered(function () {
   const templateObject = Template.instance();
-  function getAllCampaignReports() {
+
+  templateObject.getInitAllCampaignReports = function () {
+    $(".fullScreenSpin").css("display", "inline-block");
+    try {
+      getVS1Data("TCorrespendenceList").then(function (dataObject) {
+        if (dataObject.length == 0) {
+          templateObject.getAllCampaignReports();
+          $(".fullScreenSpin").css("display", "none");
+        } else {
+          let result = JSON.parse(dataObject[0].data);
+          initDatatable(result);
+          $(".fullScreenSpin").css("display", "none");
+        }
+      })
+    } catch (error) {
+      templateObject.getAllCampaignReports();
+      $(".fullScreenSpin").css("display", "none");
+    }
+  }
+  templateObject.getInitAllCampaignReports();
+
+  templateObject.getAllCampaignReports = function() {
     $(".fullScreenSpin").css("display", "inline-block");
     try {
       var erpGet = erpDb();
@@ -9,6 +30,7 @@ Template.mailchimpCampaignList.onRendered(function () {
         if (error !== undefined) {
           swal("Something went wrong!", "", "error");
         } else {
+          addVS1Data("TCorrespendenceList", JSON.stringify(result));
           initDatatable(result);
         }
         $(".fullScreenSpin").css("display", "none");
@@ -18,7 +40,7 @@ Template.mailchimpCampaignList.onRendered(function () {
       $(".fullScreenSpin").css("display", "none");
     }
   }
-  getAllCampaignReports();
+  // getAllCampaignReports();
 
   function initDatatable(data) {
     var reportArray = templateObject.makeEmailTableRows(data);
@@ -96,4 +118,28 @@ Template.mailchimpCampaignList.onRendered(function () {
     });
     return taskRows;
   };
+})
+
+
+Template.mailchimpCampaignList.events({
+  "click .btnRefresh": function (e) {
+    // Meteor._reload.reload();
+    const templateObject = Template.instance();
+    templateObject.getAllCampaignReports();
+  },
+
+  "click .printConfirm": function (event) {
+    $(".fullScreenSpin").css("display", "inline-block");
+    jQuery("#tblCampaignList_wrapper .dt-buttons .btntabletopdf").click();
+
+    $(".fullScreenSpin").css("display", "none");
+  },
+
+  "click #exportbtn": function () {
+    $(".fullScreenSpin").css("display", "inline-block");
+    jQuery("#tblCampaignList_wrapper .dt-buttons .btntabletocsv").click();
+
+    $(".fullScreenSpin").css("display", "none");
+  },
+
 })
