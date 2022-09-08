674b79b1387fd76d417933fe4b0efffcf1aeca69
FG.Zuusoft <fatih@zuusoft.com>
2022-09-08 20:33:43 +0300

    color issues fixed

--- client/reports/payrollreports/payrollhistoryreport.js
+++ client/reports/payrollreports/payrollhistoryreport.js
@@ -16,10 +16,11 @@ Template.payrollhistoryreport.onCreated(() => {
   const templateObject = Template.instance();
   templateObject.dateAsAt = new ReactiveVar();
   templateObject.reportOptions = new ReactiveVar([]);
+  templateObject.records = new ReactiveVar([]);
+
   templateObject.currencyList = new ReactiveVar([]);
   templateObject.activeCurrencyList = new ReactiveVar([]);
   templateObject.tcurrencyratehistory = new ReactiveVar([]);
-  templateObject.records = new ReactiveVar([]);
 });
 
 Template.payrollhistoryreport.onRendered(() => {
@@ -382,6 +383,63 @@ Template.payrollhistoryreport.events({
     await templateObject.setReportOptions(true);
     $(".fullScreenSpin").css("display", "none");
   },
+
+
+    // CURRENCY MODULE //
+    "click .fx-rate-btn": async (e, ui) => {
+      await loadCurrency(ui);
+      //loadCurrencyHistory();
+    },
+    "click .currency-modal-save": (e) => {
+      //$(e.currentTarget).parentsUntil(".modal").modal("hide");
+      LoadingOverlay.show();
+  
+      let templateObject = Template.instance();
+  
+      // Get all currency list
+      let _currencyList = templateObject.currencyList.get();
+  
+      // Get all selected currencies
+      const currencySelected = $(".currency-selector-js:checked");
+      let _currencySelectedList = [];
+      if (currencySelected.length > 0) {
+        $.each(currencySelected, (index, e) => {
+          const sellRate = $(e).attr("sell-rate");
+          const buyRate = $(e).attr("buy-rate");
+          const currencyCode = $(e).attr("currency");
+          const currencyId = $(e).attr("currency-id");
+          let _currency = _currencyList.find((c) => c.id == currencyId);
+          _currency.active = true;
+          _currencySelectedList.push(_currency);
+        });
+      } else {
+        let _currency = _currencyList.find((c) => c.code == defaultCurrencyCode);
+        _currency.active = true;
+        _currencySelectedList.push(_currency);
+      }
+  
+      _currencyList.forEach((value, index) => {
+        if (_currencySelectedList.some((c) => c.id == _currencyList[index].id)) {
+          _currencyList[index].active = _currencySelectedList.find(
+            (c) => c.id == _currencyList[index].id
+          ).active;
+        } else {
+          _currencyList[index].active = false;
+        }
+      });
+  
+      _currencyList = _currencyList.sort((a, b) => {
+        if (a.code == defaultCurrencyCode) {
+          return -1;
+        }
+        return 1;
+      });
+  
+      // templateObject.activeCurrencyList.set(_activeCurrencyList);
+      templateObject.currencyList.set(_currencyList);
+  
+      LoadingOverlay.hide();
+    },
 });
 
 Template.payrollhistoryreport.helpers({
