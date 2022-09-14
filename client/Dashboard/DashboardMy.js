d89cc526a33bf7c4aa968bf2940997a4cc9fba04
wangyan0349 <hmwangyan0349@hotmail.com>
2022-09-14 16:14:49 +0800

    -  Add dashboard-my, lead status setting

---
+++ client/Dashboard/DashboardMy.js
@@ -0,0 +1,34 @@
+import { ReactiveVar } from "meteor/reactive-var";
+
+Template.dashboardsalesmy.onCreated(function () {
+  this.loggedDb = new ReactiveVar("");
+  const templateObject = Template.instance();
+  templateObject.includeDashboard = new ReactiveVar();
+  templateObject.includeDashboard.set(false);
+});
+
+Template.dashboardsalesmy.onRendered(function () {
+  let templateObject = Template.instance();
+  let isDashboard = Session.get("CloudDashboardModule");
+  if (isDashboard) {
+    templateObject.includeDashboard.set(true);
+  }
+});
+
+Template.dashboardsalesmy.helpers({
+  includeDashboard: () => {
+    const res = Template.instance().includeDashboard.get();
+    return res;
+  },
+  loggedDb: function () {
+    return Template.instance().loggedDb.get();
+  },
+  loggedCompany: () => {
+    return localStorage.getItem("mySession") || "";
+  },
+});
+
+// Listen to event to update reactive variable
+Template.dashboardsalesmy.events({
+
+});
