c778833340e59b8fa145afcd0035c536e7728b11
DESKTOP-AJ0BMU7\ah1990 <yuri.bit1@mail.ru>
2022-09-14 11:35:37 -0700

    Accountant Reports update(0914)

--- client/reports/AccountantReport/accountantSelfmanagedsuperfund.js
+++ client/reports/AccountantReport/accountantSelfmanagedsuperfund.js
@@ -214,7 +214,7 @@ Template.accountant_selfmanagedsuperfund.onRendered(() => {
         }
 
         $('.fullScreenSpin').css('display', 'none');
-        templateObject.getAccountLists();
+        // templateObject.getAccountLists();
     }
     templateObject.getReceiptCategoryList();
 
@@ -485,7 +485,7 @@ Template.accountant_selfmanagedsuperfund.onRendered(() => {
                 });
             }
 
-            $("#tblAccountOverview")
+            $(".tblAccountOverview")
                 .DataTable({
                     columnDefs: [
                         // { type: 'currency', targets: 4 }
@@ -539,32 +539,30 @@ Template.accountant_selfmanagedsuperfund.onRendered(() => {
                         [0, "asc"]
                     ],
                     action: function() {
-                        $("#tblAccountOverview").DataTable().ajax.reload();
+                        $(".tblAccountOverview").DataTable().ajax.reload();
                     },
                     fnDrawCallback: function(oSettings) {
-                        setTimeout(function() {
-                            MakeNegative();
-                        }, 100);
+                        // setTimeout(function() {
+                        //     MakeNegative();
+                        // }, 100);
                     },
-                    fnInitComplete: function() {
-                        $(
-                            "<button class='btn btn-primary btnRefreshAccount' type='button' id='btnRefreshAccount' style='padding: 4px 10px; font-size: 14px; margin-left: 8px !important;'><i class='fas fa-search-plus' style='margin-right: 5px'></i>Search</button>"
-                        ).insertAfter("#tblAccountOverview_filter");
-                    },
+                    fnInitComplete: function() {},
                 })
                 .on("page", function() {
-                    setTimeout(function() {
-                        MakeNegative();
-                    }, 100);
+                    // setTimeout(function() {
+                    //     MakeNegative();
+                    // }, 100);
                     let draftRecord = templateObject.datatablerecords.get();
                     templateObject.datatablerecords.set(draftRecord);
                 })
                 .on("column-reorder", function() {})
                 .on("length.dt", function(e, settings, len) {
-                    setTimeout(function() {
-                        MakeNegative();
-                    }, 100);
+                    // setTimeout(function() {
+                    //     MakeNegative();
+                    // }, 100);
                 });
+
+            $("<button class='btn btn-primary btnRefreshAccount' type='button' id='btnRefreshAccount' style='padding: 4px 10px; font-size: 14px; margin-left: 8px !important;'><i class='fas fa-search-plus' style='margin-right: 5px'></i>Search</button>").insertAfter("#tblAccountOverview_wrapper .dataTables_filter");
             // $('.fullScreenSpin').css('display','none');
         }, 10);
 
--- client/reports/AccountantReport/accountantSingledirector.js
+++ client/reports/AccountantReport/accountantSingledirector.js
@@ -215,7 +215,7 @@ Template.accountant_singledirector.onRendered(() => {
         }
 
         $('.fullScreenSpin').css('display', 'none');
-        templateObject.getAccountLists();
+        // templateObject.getAccountLists();
     }
     templateObject.getReceiptCategoryList();
 
@@ -486,7 +486,7 @@ Template.accountant_singledirector.onRendered(() => {
                 });
             }
 
-            $("#tblAccountOverview")
+            $(".tblAccountOverview")
                 .DataTable({
                     columnDefs: [
                         // { type: 'currency', targets: 4 }
@@ -540,32 +540,30 @@ Template.accountant_singledirector.onRendered(() => {
                         [0, "asc"]
                     ],
                     action: function() {
-                        $("#tblAccountOverview").DataTable().ajax.reload();
+                        $(".tblAccountOverview").DataTable().ajax.reload();
                     },
                     fnDrawCallback: function(oSettings) {
-                        setTimeout(function() {
-                            MakeNegative();
-                        }, 100);
+                        // setTimeout(function() {
+                        //     MakeNegative();
+                        // }, 100);
                     },
-                    fnInitComplete: function() {
-                        $(
-                            "<button class='btn btn-primary btnRefreshAccount' type='button' id='btnRefreshAccount' style='padding: 4px 10px; font-size: 14px; margin-left: 8px !important;'><i class='fas fa-search-plus' style='margin-right: 5px'></i>Search</button>"
-                        ).insertAfter("#tblAccountOverview_filter");
-                    },
+                    fnInitComplete: function() {},
                 })
                 .on("page", function() {
-                    setTimeout(function() {
-                        MakeNegative();
-                    }, 100);
+                    // setTimeout(function() {
+                    //     MakeNegative();
+                    // }, 100);
                     let draftRecord = templateObject.datatablerecords.get();
                     templateObject.datatablerecords.set(draftRecord);
                 })
                 .on("column-reorder", function() {})
                 .on("length.dt", function(e, settings, len) {
-                    setTimeout(function() {
-                        MakeNegative();
-                    }, 100);
+                    // setTimeout(function() {
+                    //     MakeNegative();
+                    // }, 100);
                 });
+
+            $("<button class='btn btn-primary btnRefreshAccount' type='button' id='btnRefreshAccount' style='padding: 4px 10px; font-size: 14px; margin-left: 8px !important;'><i class='fas fa-search-plus' style='margin-right: 5px'></i>Search</button>").insertAfter("#tblAccountOverview_wrapper .dataTables_filter");
             // $('.fullScreenSpin').css('display','none');
         }, 10);
 
