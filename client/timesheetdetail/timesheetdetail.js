5fe35da1e69db4e3ead4ad93c42b46173419cb56
FG.Zuusoft <fatih@zuusoft.com>
2022-10-07 12:54:35 +0300

    timesheet update

--- client/timesheetdetail/timesheetdetail.js
+++ client/timesheetdetail/timesheetdetail.js
@@ -1,10 +1,10 @@
-import { ContactService } from "../contacts/contact-service";
-import { ReactiveVar } from "meteor/reactive-var";
-import { CoreService } from "../js/core-service";
-import { UtilityService } from "../utility-service";
-import { ProductService } from "../product/product-service";
+import {ContactService} from "../contacts/contact-service";
+import {ReactiveVar} from "meteor/reactive-var";
+import {CoreService} from "../js/core-service";
+import {UtilityService} from "../utility-service";
+import {ProductService} from "../product/product-service";
 import XLSX from "xlsx";
-import { SideBarService } from "../js/sidebar-service";
+import {SideBarService} from "../js/sidebar-service";
 import "jquery-editable-select";
 import draggableCharts from "../js/Charts/draggableCharts";
 import resizableCharts from "../js/Charts/resizableCharts";
@@ -18,50 +18,100 @@ import LoadingOverlay from "../LoadingOverlay";
 import PayRun from "../js/Api/Model/PayRun";
 import CachedHttp from "../lib/global/CachedHttp";
 import erpObject from "../lib/global/erp-objects";
-import { EmployeeFields } from "../js/Api/Model/Employee";
+import {EmployeeFields} from "../js/Api/Model/Employee";
+import {ReportService} from "../reports/report-service";
 let sideBarService = new SideBarService();
 let utilityService = new UtilityService();
-
 
 Template.timesheetdetail.onCreated(function () {
   this.timesheet = new ReactiveVar();
   this.timeSheetList = new ReactiveVar([]);
+  this.employee = new ReactiveVar();
 });
 
 Template.timesheetdetail.onRendered(function () {
-
-
-   this.loadTimeSheet = async () => {
+  this.loadTimeSheet = async () => {
     let id = FlowRouter.current().queryParams.tid;
 
     let data = await CachedHttp.get(erpObject.TTimeSheetEntry, async () => {
-      return await (new ContactService()).getAllTimeSheetList();
+      return await new ContactService().getAllTimeSheetList();
     }, {
-      useIndexDb: true, 
-      useLocalStorage:false,
+      useIndexDb: true,
+      useLocalStorage: false,
       fallBackToLocal: true,
-      validate: (cachedResponse) => {
+      validate: cachedResponse => {
         return false;
+      }
+    });
+    data = data.response;
+
+    let timesheets = data.ttimesheet.map(t => t.fields);
+     timesheets.forEach((t, index) => {
+      if(t.Status == "") {
+        t.Status = "Draft";
+      }
+    })
+    let timesheet = timesheets.find(o => o.ID == id);
+
+    console.log("timsheet", timesheet);
+    this.timesheet.set(timesheet);
+  };
+
+  this.loadEmployee = async () => {
+    let timesheet = await this.timesheet.get();
+
+    let data = await CachedHttp.get(erpObject.TEmployee, async () => {
+      return await new ContactService().getAllEmployees();
+    }, {
+      useIndexDb: true,
+      useLocalStorage: false,
+      fallBackToLocal: true,
+      validate: cachedResponse => {
+        return true;
       }
     });
 
     data = data.response;
+    let employees = data.temployee.map(e => e.fields);
 
-    let timesheets = data.ttimesheet.map(t => t.fields);
-    let timesheet = data.ttimesheet.find(o => o.ID == id)
-    console.log('timesheets', timesheet);
- 
+    const selectedEmployee = employees.find(e => e.EmployeeName == timesheet.EmployeeName);
+    this.employee.set(selectedEmployee);
 
-   }
+    console.log("employees", employees);
+    console.log("employee", selectedEmployee);
+  };
 
-   this.loadTimeSheet();
-    
+  //  this.loadTimeSheetEntry = async () => {
+  //   let data = await CachedHttp.get(erpObject.TTimeSheetEntry, async () => {
+  //     return await (new ReportService()).getTimeSheetEntry();
+  //   }, {
+  //     useIndexDb: true,
+  //     useLocalStorage: false,
+  //     fallBackToLocal: true,
+  //     validate: (cachedResponse) => {
+  //       return true;  Shouldn't return hard codedly, but only if the data is ok
+  //     }
+  //   });
+
+  //   data = data.response;
+
+  //   console.log("data", data);
+
+  //  }
+
+  this.initPage = async () => {
+    LoadingOverlay.show();
+    await this.loadTimeSheet();
+    await this.loadEmployee();
+
+    LoadingOverlay.hide();
+  };
+  this.initPage();
 });
 
 Template.timesheetdetail.events({
   "click .btnAddNewLine": function () {
-    $("#tblTimeSheetInner tbody").find('tr:last').prev().after(
-        `<tr>
+    $("#tblTimeSheetInner tbody").find("tr:last").prev().after(`<tr>
         <td>
          <select class="form-control">
              <option>Select</option>
@@ -77,15 +127,21 @@ Template.timesheetdetail.events({
         <td>0.00</td>
         <td><button type="button" class="btn btn-danger btn-rounded btn-sm btnDeletePayslip" data-id="1" autocomplete="off"><i class="fa fa-remove"></i></button></td>
      </tr>
-        `
-    )
+        `);
   },
   "click .btnDeleteRow": function (e) {
-    $(e.target).parents('tr').remove();
-  },
-  
+    $(e.target).parents("tr").remove();
+  }
 });
 
 Template.timesheetdetail.helpers({
- 
+  formatDate: date => {
+    return moment(date).format("D MMM YYYY");
+  },
+  timesheet: () => {
+    return Template.instance().timesheet.get();
+  },
+  employee: () => {
+    return Template.instance().employee.get();
+  }
 });
