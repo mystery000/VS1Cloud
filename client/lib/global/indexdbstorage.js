cb0a3c627115b90d95d8a8cf897a08737c93a7db
trueerp_cloud <devsa@trueerp.com>
2022-10-21 18:23:29 +0200

    Rasheed VS1 Build

--- client/lib/global/indexdbstorage.js
+++ client/lib/global/indexdbstorage.js
@@ -572,7 +572,7 @@ getStoreToDelete = async function(email) {
 openDbCheckVersion = async function() {
     var promiseversion = new Promise((resolve, reject) => {
         var versionExists = false;
-        let dbReqVersion = indexedDB.open('TDatabaseVersion', 91);
+        let dbReqVersion = indexedDB.open('TDatabaseVersion', 92);
         dbReqVersion.onsuccess = function() {
             resolve(versionExists);
         };
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const HTMLToCache = '/';
-const version = 'MSW V0.202';
+const version = 'MSW V0.203';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(version).then((cache) => {
