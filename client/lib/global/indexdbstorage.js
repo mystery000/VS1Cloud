4dc01d86d7e316c4d8b6c3fadd79c4596f2b6f86
trueerp_cloud <devsa@trueerp.com>
2022-11-02 13:35:10 +0200

    Rasheed Commit Report Template

--- client/lib/global/indexdbstorage.js
+++ client/lib/global/indexdbstorage.js
@@ -581,7 +581,7 @@ getStoreToDelete = async function(email) {
 openDbCheckVersion = async function() {
     var promiseversion = new Promise((resolve, reject) => {
         var versionExists = false;
-        let dbReqVersion = indexedDB.open('TDatabaseVersion', 94);
+        let dbReqVersion = indexedDB.open('TDatabaseVersion', 95);
         dbReqVersion.onsuccess = function() {
             resolve(versionExists);
         };
---
+++ client/vs1_templates/date_picker/daterangefromto.js
@@ -0,0 +1,48 @@
+import moment from "moment";
+Template.daterangefromto.onCreated(function(){
+});
+
+Template.daterangefromto.onRendered(function() {
+  var today = moment().format("DD/MM/YYYY");
+  var currentDate = new Date();
+  var begunDate = moment(currentDate).format("DD/MM/YYYY");
+  let fromDateMonth = currentDate.getMonth() + 1;
+  let fromDateDay = currentDate.getDate();
+  if (currentDate.getMonth() + 1 < 10) {
+    fromDateMonth = "0" + (currentDate.getMonth() + 1);
+  }
+
+  let prevMonth = moment().subtract(1, 'months').format('MM')
+  if (currentDate.getDate() < 10) {
+    fromDateDay = "0" + currentDate.getDate();
+  }
+  var fromDate = fromDateDay + "/" + prevMonth + "/" + currentDate.getFullYear();
+  $(".dateTo,.dateFrom").datepicker({
+    showOn: "button",
+    buttonText: "Show Date",
+    buttonImageOnly: true,
+    buttonImage: "/img/imgCal2.png",
+    dateFormat: "dd/mm/yy",
+    showOtherMonths: true,
+    selectOtherMonths: true,
+    changeMonth: true,
+    changeYear: true,
+    yearRange: "-90:+10",
+    onChangeMonthYear: function (year, month, inst) {
+      // Set date to picker
+      $(this).datepicker("setDate",
+        new Date(year, inst.selectedMonth, inst.selectedDay)
+      );
+    },
+  });
+
+  $("#dateFrom").val(fromDate);
+  $("#dateTo").val(begunDate);
+});
+
+Template.daterangefromto.events({
+
+});
+
+Template.daterangefromto.helpers({
+});
