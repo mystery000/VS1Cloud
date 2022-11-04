05099eea961572ef88968a55304d5d691f7d39b0
bruno@vs1cloud.com <bruno@vs1cloud.com>
2022-11-04 15:37:13 +0200

    Tinyiko Miracle Mabale 04/11/2022 Worklog
    #Payment Method Settings:
    - Redesigned Payment method settings page
    - Converting existing format to template format
    - Buttons (Refresh, Add New, Export, Import, Print & Display Settings, Save and Delete)
    - Forms (Add, Edit & Delete) //Delete function with error
    - Table (Retrieve, Update, Horizontal Scrollable)
    - Changing checkbox to checkbox-swicth for credit cards options
    
    #Terms Settings:
    - Redesigned Payment method settings page
    - Converting existing format to template format
    - Buttons (Refresh, Add New, Export, Import, Print & Display Settings, Save and Delete)
    - Forms (Add, Edit & Delete) //Delete function with error
    - Table (Retrieve, Update, Horizontal Scrollable)
    - Changing checkbox to checkbox-swicth for credit cards options

--- client/lib/global/indexdbstorage.js
+++ client/lib/global/indexdbstorage.js
@@ -48,6 +48,7 @@ openDb = function(dbName) {
             db.createObjectStore("TTaxcodeVS1", { keyPath: "EmployeeEmail" });
             db.createObjectStore("TSubTaxVS1", { keyPath: "EmployeeEmail" });
             db.createObjectStore("TTermsVS1", { keyPath: "EmployeeEmail" });
+            db.createObjectStore("TTermsVS1List", { keyPath: "EmployeeEmail" });
             db.createObjectStore("TDeptClass", { keyPath: "EmployeeEmail" });
             db.createObjectStore("TDepartment", { keyPath: "EmployeeEmail" });
             db.createObjectStore("TCurrency", { keyPath: "EmployeeEmail" });
