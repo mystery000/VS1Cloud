e1501c971c651977f3e78cdc691b31ec7ed3f31f
FG.Zuusoft <fatih@zuusoft.com>
2022-11-09 12:54:16 +0300

    removing a text in swal

--- client/settings/currencies-setting/currencies.js
+++ client/settings/currencies-setting/currencies.js
@@ -1557,7 +1557,7 @@ export const updateAllCurrencies = (employeeId,
         const result = await swal({
           title: "Update in progress",
          // text: "Do you wish to add an account ?",
-          type: "question",
+          type: "success",
           showCancelButton: false,
           confirmButtonText: "Ok",
         });
