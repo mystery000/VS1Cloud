438ea79c8b4d88ecbc7517df5a1ce4702f1017ec
CleanCrazy-dev <yuraivanov.dev@gmail.com>
2022-12-07 23:47:52 +0300

    added filtered table

--- client/contacts/addSupplier.js
+++ client/contacts/addSupplier.js
@@ -2725,7 +2725,6 @@ Template.supplierscard.helpers({
         if (temp && temp.mobile) {
             temp.mobile = temp.mobile.replace('+61', '0')
         }
-        console.log('temp:',temp)
         return temp;
     },
     countryList: () => {
