b9d573415304f8e6f38a9e8e92aaeac62ad5cac4
oleksandr <lkostyrka17@gmail.com>
2022-12-13 17:02:06 +0900

    Replace billcard with transaction template

--- client/vs1_templates/transaction_temp/transaction_line.js
+++ client/vs1_templates/transaction_temp/transaction_line.js
@@ -133,24 +133,28 @@ Template.transaction_line.onRendered(function() {
       let reset_data = [
           { index: 0, label: "Product Name", class: "ProductName", width: "300", active: true, display: true },
           { index: 1, label: "Description", class: "Description", width: "", active: true, display: true },
-          { index: 2, label: "Qty", class: "Qty", width: "50", active: true, display: true },
-          { index: 3, label: "Ordered", class: "Ordered", width: "75", active: true, display: true },
-          { index: 4, label: "Shipped", class: "Shipped", width: "75", active: true, display: true },
-          { index: 5, label: "BO", class: "BackOrder", width: "75", active: true, display: true },
-          { index: 6, label: "Serial/Lot No", class: "SerialNo", width: "100", active: true, display: true },
-          { index: 7, label: "Fixed Asset", class: "FixedAsset", width: "100", active: true, display: true },
-          { index: 8, label: "Customer/Job", class: "CustomerJob", width: "110", active: true, display: true },
-          { index: 9, label: "Unit Price (Ex)", class: "UnitPriceEx", width: "125", active: true, display: true },
-          { index: 10, label: "Unit Price (Inc)", class: "UnitPriceInc", width: "125", active: false, display: true },
-          { index: 11, label: "Cost Price", class: "CostPrice", width: "110", active: true, display: true },
-          { index: 12, label: "Disc %", class: "Discount", width: "75", active: true, display: true },
-          { index: 13, label: "CustField1", class: "SalesLinesCustField1", width: "110", active: true, display: true },
-          { index: 14, label: "Tax Rate", class: "TaxRate", width: "91", active: true, display: true },
-          { index: 15, label: "Tax Code", class: "TaxCode", width: "95", active: true, display: true },
-          { index: 16, label: "Tax Amt", class: "TaxAmount", width: "75", active: true, display: true },
-          { index: 17, label: "Amount (Ex)", class: "AmountEx", width: "120", active: true, display: true },
-          { index: 18, label: "Amount (Inc)", class: "AmountInc", width: "120", active: false, display: true },
-          { index: 19, label: "Units", class: "Units", width: "95", active: true, display: true },
+          { index: 2, label: "Account Name", class: "AccountName", width: "300", active: true, display: true },
+          { index: 3, label: "Memo", class: "Memo", width: "", active: true, display: true },
+          { index: 4, label: "Qty", class: "Qty", width: "50", active: true, display: true },
+          { index: 5, label: "Ordered", class: "Ordered", width: "75", active: true, display: true },
+          { index: 6, label: "Shipped", class: "Shipped", width: "75", active: true, display: true },
+          { index: 7, label: "BO", class: "BackOrder", width: "75", active: true, display: true },
+          { index: 8, label: "Serial/Lot No", class: "SerialNo", width: "100", active: true, display: true },
+          { index: 9, label: "Fixed Asset", class: "FixedAsset", width: "100", active: true, display: true },
+          { index: 10, label: "Customer/Job", class: "CustomerJob", width: "110", active: true, display: true },
+          { index: 11, label: "Unit Price (Ex)", class: "UnitPriceEx", width: "152", active: true, display: true },
+          { index: 12, label: "Unit Price (Inc)", class: "UnitPriceInc", width: "152", active: false, display: true },
+          { index: 13, label: "Cost Price", class: "CostPrice", width: "110", active: true, display: true },
+          { index: 14, label: "Disc %", class: "Discount", width: "75", active: true, display: true },
+          { index: 15, label: "CustField1", class: "SalesLinesCustField1", width: "110", active: true, display: true },
+          { index: 16, label: "Tax Rate", class: "TaxRate", width: "91", active: true, display: true },
+          { index: 17, label: "Tax Code", class: "TaxCode", width: "95", active: true, display: true },
+          { index: 18, label: "Tax Amt", class: "TaxAmount", width: "75", active: true, display: true },
+          { index: 19, label: "Amount (Ex)", class: "AmountEx", width: "152", active: true, display: true },
+          { index: 20, label: "Amount (Inc)", class: "AmountInc", width: "152", active: false, display: true },
+          { index: 21, label: "Units", class: "cc", width: "95", active: true, display: true },
+          { index: 22, label: "Custom Field 1", class: "CustomField1", width: "124", active: false, display: false },
+          { index: 23, label: "Custom Field 2", class: "CustomField2", width: "124", active: false, display: false },
       ];
       let isBatchSerialNoTracking = Session.get("CloudShowSerial") || false;
       let isBOnShippedQty = Session.get("CloudSalesQtyOnly");
@@ -197,13 +201,14 @@ Template.transaction_line.onRendered(function() {
             { index: 9, label: "Tax Code", class: "TaxCode", width: "95", active: true, display: true },
             { index: 10, label: "Tax Amt", class: "TaxAmount", width: "95", active: true, display: true },
             { index: 11, label: "Serial/Lot No", class: "SerialNo", width: "124", active: true, display: true },
-            { index: 12, label: "Amount (Ex)", class: "AmountEx", width: "140", active: true, display: true },
-            { index: 13, label: "Amount (Inc)", class: "AmountInc", width: "140", active: false, display: true },
+            { index: 12, label: "Amount (Ex)", class: "AmountEx", width: "152", active: true, display: true },
+            { index: 13, label: "Amount (Inc)", class: "AmountInc", width: "152", active: false, display: true },
           ];
             reset_data = reset_data.map( data => {
             x = reset_data_salesorder.find( x => x.class === data.class);
             if(x != undefined) {
                 x.index = data.index; 
+                x.width = data.width; 
                 return x;
             } else {
                 data.active = false;
@@ -223,6 +228,7 @@ Template.transaction_line.onRendered(function() {
                         x = reset_data.find( x => x.class === data.class);
                         if(x != undefined) {
                             x.index = data.index; 
+                            x.width = data.width; 
                             return x;
                         } else {
                             data.active = false;
@@ -241,7 +247,8 @@ Template.transaction_line.onRendered(function() {
                               reset_data = templateObject.reset_data.get().map( data => {
                                 x = reset_data.find( x => x.class === data.class);
                                 if(x != undefined) {
-                                    x.index = data.index; 
+                                    x.index = data.index;
+                                    x.width = data.width; 
                                     return x;
                                 } else {
                                     data.active = false;
@@ -249,7 +256,6 @@ Template.transaction_line.onRendered(function() {
                                     return data;
                                 }               
                               })
-                              
                               templateObject.showCustomFieldDisplaySettings(reset_data);
                           }
                       }
@@ -445,8 +451,22 @@ Template.transaction_line.helpers({
             fixedAsset.active = false;
         }
     }
-    
-
+    let canShowUOM = Template.instance().data.canShowUOM;
+    let isSerialNoTracking = Template.instance().data.isBatchSerialNoTracking;
+    if(!canShowUOM) {
+        let units = data.find((x) => x.class === 'Units');
+        if( units != undefined) {
+            units.display = false;
+            units.active = false;
+        }
+    }
+    if(!isSerialNoTracking) {
+        let serialNo = data.find((x) => x.class === 'SerialNo');
+        if( serialNo != undefined) {
+            serialNo.display = false;
+            serialNo.active = false;
+        }
+    }
 
     return data;
   },
