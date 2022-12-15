7ee26625452b73ca6d230b4ccb9ce54d9f17f90c
Kelvin <milenkobitbucket@outlook.com>
2022-12-15 09:30:59 +0800

    Update the transaction footers

--- client/vs1_templates/transaction_temp/transaction_footer/index.js
+++ client/vs1_templates/transaction_temp/transaction_footer/index.js
@@ -1,9 +1,9 @@
 import 'jquery-ui-dist/external/jquery/jquery';
 import 'jquery-ui-dist/jquery-ui';
 
-Template.transaction_buttons_bottom.onCreated( function () {});
-Template.transaction_buttons_bottom.onRendered( function () {});
-Template.transaction_buttons_bottom.events({
+Template.transaction_footer.onCreated( function () {});
+Template.transaction_footer.onRendered( function () {});
+Template.transaction_footer.events({
   "click #open_print_confirm": function (event) {
     playPrintAudio();
     setTimeout(async function() {
