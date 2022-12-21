286cdec8ade9974f2c91b8cbd460f4a8df20009b
oleksandr <lkostyrka17@gmail.com>
2022-12-22 08:12:45 +0900

    Update transactions tables.

--- client/js/new_salesorder.js
+++ client/js/new_salesorder.js
@@ -79,9 +79,6 @@ Template.new_salesorder.onCreated(function () {
   this.uploadedFile = new ReactiveVar();
   this.uploadedFiles = new ReactiveVar([]);
   this.attachmentCount = new ReactiveVar();
-  this.displayfields = new ReactiveVar([]);
-  this.reset_data = new ReactiveVar([]);
-
   this.address = new ReactiveVar();
   this.abn = new ReactiveVar();
   this.referenceNumber = new ReactiveVar();
@@ -302,134 +299,7 @@ Template.new_salesorder.onRendered(function () {
     yearRange: "-90:+10",
   });
 
-  // set initial table rest_data
-  function init_reset_data() {
-    let reset_data = [
-      {
-        index: 0,
-        label: "Product Name",
-        class: "ProductName",
-        width: "300",
-        active: true,
-        display: true,
-      },
-      {
-        index: 1,
-        label: "Description",
-        class: "Description",
-        width: "",
-        active: true,
-        display: true,
-      },
-      {
-        index: 2,
-        label: "Qty",
-        class: "Qty",
-        width: "55",
-        active: true,
-        display: true,
-      },
-      {
-        index: 3,
-        label: "Unit Price (Ex)",
-        class: "UnitPriceEx",
-        width: "152",
-        active: true,
-        display: true,
-      },
-      {
-        index: 4,
-        label: "Unit Price (Inc)",
-        class: "UnitPriceInc",
-        width: "152",
-        active: false,
-        display: true,
-      },
-      {
-        index: 5,
-        label: "Disc %",
-        class: "Discount",
-        width: "95",
-        active: true,
-        display: true,
-      },
-      {
-        index: 6,
-        label: "Cost Price",
-        class: "CostPrice",
-        width: "110",
-        active: false,
-        display: true,
-      },
-      {
-        index: 7,
-        label: "SalesLines CustField1",
-        class: "SalesLinesCustField1",
-        width: "110",
-        active: false,
-        display: true,
-      },
-      {
-        index: 8,
-        label: "Tax Rate",
-        class: "TaxRate",
-        width: "95",
-        active: false,
-        display: true,
-      },
-      {
-        index: 9,
-        label: "Tax Code",
-        class: "TaxCode",
-        width: "95",
-        active: true,
-        display: true,
-      },
-      {
-        index: 10,
-        label: "Tax Amt",
-        class: "TaxAmount",
-        width: "95",
-        active: true,
-        display: true,
-      },
-      {
-        index: 11,
-        label: "Serial/Lot No",
-        class: "SerialNo",
-        width: "124",
-        active: true,
-        display: true,
-      },
-      {
-        index: 12,
-        label: "Amount (Ex)",
-        class: "AmountEx",
-        width: "140",
-        active: true,
-        display: true,
-      },
-      {
-        index: 13,
-        label: "Amount (Inc)",
-        class: "AmountInc",
-        width: "140",
-        active: false,
-        display: true,
-      },
-    ];
 
-    let isBatchSerialNoTracking = Session.get("CloudShowSerial") || false;
-    if (isBatchSerialNoTracking) {
-      reset_data[11].display = true;
-    } else {
-      reset_data[11].display = false;
-    }
-
-    let templateObject = Template.instance();
-    templateObject.reset_data.set(reset_data);
-  }
-  init_reset_data();
   // set initial table rest_data
 
   // templateObject.getTemplateInfoNew = function(){
@@ -10224,62 +10094,7 @@ Template.new_salesorder.onRendered(function () {
 
   tempObj.getSubTaxCodes();
 
-  // custom field displaysettings
-  tempObj.initCustomFieldDisplaySettings = function (data, listType) {
-    let templateObject = Template.instance();
-    let reset_data = templateObject.reset_data.get();
-    showCustomFieldDisplaySettings(reset_data);
 
-    try {
-      getVS1Data("VS1_Customize").then(function (dataObject) {
-        if (dataObject.length == 0) {
-          sideBarService
-            .getNewCustomFieldsWithQuery(
-              parseInt(Session.get("mySessionEmployeeLoggedID")),
-              listType
-            )
-            .then(function (data) {
-              reset_data = data.ProcessLog.Obj.CustomLayout[0].Columns;
-              showCustomFieldDisplaySettings(reset_data);
-            })
-            .catch(function (err) {});
-        } else {
-          let data = JSON.parse(dataObject[0].data);
-          if (data.ProcessLog.Obj.CustomLayout.length > 0) {
-            for (let i = 0; i < data.ProcessLog.Obj.CustomLayout.length; i++) {
-              if (data.ProcessLog.Obj.CustomLayout[i].TableName == listType) {
-                reset_data = data.ProcessLog.Obj.CustomLayout[i].Columns;
-                showCustomFieldDisplaySettings(reset_data);
-              }
-            }
-          }
-          // handle process here
-        }
-      });
-    } catch (error) {}
-    return;
-  };
-
-  function showCustomFieldDisplaySettings(reset_data) {
-    let custFields = [];
-    let customData = {};
-    let customFieldCount = reset_data.length;
-
-    for (let r = 0; r < customFieldCount; r++) {
-      customData = {
-        active: reset_data[r].active,
-        id: reset_data[r].index,
-        custfieldlabel: reset_data[r].label,
-        class: reset_data[r].class,
-        display: reset_data[r].display,
-        width: reset_data[r].width ? reset_data[r].width : "",
-      };
-      custFields.push(customData);
-    }
-    tempObj.displayfields.set(custFields);
-  }
-
-  tempObj.initCustomFieldDisplaySettings("", "tblSalesOrderLine");
 
   tempObj.checkAbleToMakeWorkOrder = function () {
     let bomProducts = localStorage.getItem("TProcTree")
@@ -10653,30 +10468,9 @@ Template.new_salesorder.helpers({
   getDefaultCurrency: () => {
     return defaultCurrencyCode;
   },
-  convertToForeignAmount: (amount) => {
-    return convertToForeignAmount(
-      amount,
-      $("#exchange_rate").val(),
-      getCurrentCurrencySymbol()
-    );
-  },
 
-  displayFieldColspan: (displayfield) => {
-    if (foreignCols.includes(displayfield.custfieldlabel)) {
-      if (Template.instance().isForeignEnabled.get() == true) {
-        return 2;
-      }
-      return 1;
-    }
-    return 1;
-  },
+ 
 
-  subHeaderForeign: (displayfield) => {
-    if (foreignCols.includes(displayfield.custfieldlabel)) {
-      return true;
-    }
-    return false;
-  },
   abletomakeworkorder: () => {
     return Template.instance().abletomakeworkorder.get();
   },
@@ -15025,135 +14819,6 @@ Template.new_salesorder.events({
     let columHeaderUpdate = $(event.target).attr("valueupdate");
     $("th.col" + columHeaderUpdate + "").html(columData);
     // $("" + columHeaderUpdate + "").html(columData);
-  },
-
-  // custom field displaysettings
-  "click .btnSaveGridSettings": async function (event) {
-    playSaveAudio();
-    let templateObject = Template.instance();
-    setTimeout(async function () {
-      let lineItems = [];
-      $(".fullScreenSpin").css("display", "inline-block");
-
-      $(".displaySettings").each(function (index) {
-        var $tblrow = $(this);
-        var fieldID = $tblrow.attr("custid") || 0;
-        var colTitle = $tblrow.find(".divcolumn").text() || "";
-        var colWidth = $tblrow.find(".custom-range").val() || 0;
-        var colthClass = $tblrow.find(".divcolumn").attr("valueupdate") || "";
-        var colHidden = false;
-        if ($tblrow.find(".custom-control-input").is(":checked")) {
-          colHidden = true;
-        } else {
-          colHidden = false;
-        }
-        let lineItemObj = {
-          index: parseInt(fieldID),
-          label: colTitle,
-          active: colHidden,
-          width: parseInt(colWidth),
-          class: colthClass,
-          display: true,
-        };
-
-        lineItems.push(lineItemObj);
-      });
-
-      let reset_data = templateObject.reset_data.get();
-      reset_data = reset_data.filter((redata) => redata.display == false);
-      lineItems.push(...reset_data);
-      lineItems.sort((a, b) => a.index - b.index);
-
-      try {
-        let erpGet = erpDb();
-        let tableName = "tblSalesOrderLine";
-        let employeeId =
-          parseInt(Session.get("mySessionEmployeeLoggedID")) || 0;
-        let added = await sideBarService.saveNewCustomFields(
-          erpGet,
-          tableName,
-          employeeId,
-          lineItems
-        );
-        $(".fullScreenSpin").css("display", "none");
-        if (added) {
-          sideBarService
-            .getNewCustomFieldsWithQuery(
-              parseInt(Session.get("mySessionEmployeeLoggedID")),
-              ""
-            )
-            .then(function (dataCustomize) {
-              addVS1Data("VS1_Customize", JSON.stringify(dataCustomize));
-            });
-          swal({
-            title: "SUCCESS",
-            text: "Display settings is updated!",
-            type: "success",
-            showCancelButton: false,
-            confirmButtonText: "OK",
-          }).then((result) => {
-            if (result.value) {
-              $("#myModal2").modal("hide");
-            }
-          });
-        } else {
-          swal("Something went wrong!", "", "error");
-        }
-      } catch (error) {
-        $(".fullScreenSpin").css("display", "none");
-        swal("Something went wrong!", "", "error");
-      }
-    }, delayTimeAfterSound);
-  },
-
-  // custom field displaysettings
-  "click .btnResetGridSettings": function (event) {
-    let templateObject = Template.instance();
-    let reset_data = templateObject.reset_data.get();
-    let isBatchSerialNoTracking = Session.get("CloudShowSerial") || false;
-    if (isBatchSerialNoTracking) {
-      reset_data[11].display = true;
-    } else {
-      reset_data[11].display = false;
-    }
-    reset_data = reset_data.filter((redata) => redata.display);
-
-    $(".displaySettings").each(function (index) {
-      let $tblrow = $(this);
-      $tblrow.find(".divcolumn").text(reset_data[index].label);
-      $tblrow
-        .find(".custom-control-input")
-        .prop("checked", reset_data[index].active);
-
-      let title = $("#tblSalesOrderLine").find("th").eq(index);
-      if (
-        reset_data[index].class === "AmountEx" ||
-        reset_data[index].class === "UnitPriceEx"
-      ) {
-        $(title).html(
-          reset_data[index].label + `<i class="fas fa-random fa-trans"></i>`
-        );
-      } else if (
-        reset_data[index].class === "AmountInc" ||
-        reset_data[index].class === "UnitPriceInc"
-      ) {
-        $(title).html(
-          reset_data[index].label + `<i class="fas fa-random"></i>`
-        );
-      } else {
-        $(title).html(reset_data[index].label);
-      }
-
-      if (reset_data[index].active) {
-        $(".col" + reset_data[index].class).addClass("showColumn");
-        $(".col" + reset_data[index].class).removeClass("hiddenColumn");
-      } else {
-        $(".col" + reset_data[index].class).addClass("hiddenColumn");
-        $(".col" + reset_data[index].class).removeClass("showColumn");
-      }
-      $(".rngRange" + reset_data[index].class).val(reset_data[index].width);
-      $(".col" + reset_data[index].class).css("width", reset_data[index].width);
-    });
   },
 
   "click .btnResetSettings": function (event) {
