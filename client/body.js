17127b7fc5b9cb6875f43afdf4875d4265c76007
Tinyiko Miracle Mabale <mabale@vs1cloud.com>
2023-01-03 16:58:42 +0200

    Tinyiko More imports from DevTop & Matthias

--- client/body.js
+++ client/body.js
@@ -273,16 +273,41 @@ import './contacts/addEmployee.js';
 import './settings/subscription-settings/subscription.js';
 import './contacts/popemployeelist.js';
 import './overviews/accountsoverview.js';
-import './settings/tax-rates-setting/taxratelistpop.js';
 import './overviews/Modal/AddAccountModal.js';
 import './eft/optionsModal/eftOptionsModal.js';
 import './eft/bankCodesModal/eftBankCodesModal.js';
 import './vs1charts/accountrevenuestreams/accountrevenuestreams.js';
+/* Tinyiko */
 
 /* Rasheed */
-import './overviews/bankingoverview.js';
 import { DataTable } from 'datatables.net';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+/* Rasheed */
+
+/* DevTop/Dev Pioneer */
+import './vs1_templates/report/accountant_header.js';
+import './reports/AccountantReport/accountantCompany.js';
+import './reports/AccountantReport/accountantCompanyastrustee.js';
+import './reports/AccountantReport/accountantFinancialstatement.js';
+import './reports/AccountantReport/accountantIndividual.js';
+import './reports/AccountantReport/accountantPartnershipnontrading.js';
+import './reports/AccountantReport/accountantSelfmanagedsuperfund.js';
+import './reports/AccountantReport/accountantSingledirector.js';
+import './reports/AccountantReport/accountantSoletradernontrading.js';
+import './reports/AccountantReport/accountantTrust.js';
+import './reports/AccountantReport/accountantTrustnontrading.js';
+import './reports/allreports.js';
+/* DevTop/Dev Pioneer */
+
+/* Matthias */
+import './inventory/inventory.js';
+import './inventory/inventorylist.js';
+import './inventory/productlist.js';
+import './inventory/productview.js';
+import './inventory/inventorypopups/onBackOrderPopUp.js';
+import './inventory/inventorypopups/onOrderPopUp.js';
+import './inventory/inventorypopups/onSalesOrderPopUp.js';
+/* Matthias */
 
 //import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
