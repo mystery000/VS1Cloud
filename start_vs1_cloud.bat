4fd96d6131ee728042a5b4eb3e8bfeb2da143d05
LovelyDev829 <lovelydeveloper829@gmail.com>
2023-01-09 01:44:12 -0800

    modified start_vs1_cloud.bat

--- start_vs1_cloud.bat
+++ start_vs1_cloud.bat
@@ -1,4 +1,6 @@
-:: c:\ path to project
-cd D:\Dene\vs1_cloud_project
+cd..
+cd..
+D:
+cd D:\Temp_Folder\vs1_cloud_project
 set TOOL_NODE_FLAGS=--max-old-space-size=8000
-meteor --port 3000 
+meteor --port 3000
