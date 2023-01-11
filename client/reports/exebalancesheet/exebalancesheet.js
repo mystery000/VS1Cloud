54e94cd7b3d063c129bf11be1132e15c84f7d5a7
wonabkevin123 <wonabkevin@gmail.com>
2023-01-11 18:57:32 -0500

    Import of files and fixes on Dashboard Executive and its reports.

--- client/reports/exebalancesheet/exebalancesheet.js
+++ client/reports/exebalancesheet/exebalancesheet.js
@@ -8,6 +8,10 @@ import { TaxRateService } from "../../settings/settings-service";
 import FxGlobalFunctions from "../../packages/currency/FxGlobalFunctions";
 import Datehandler from "../../DateHandler";
 
+import { Template } from 'meteor/templating';
+import './exebalancesheet.html';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
 let defaultCurrencyCode = CountryAbbr; // global variable "AUD"
 
 let reportService = new ReportService();
@@ -55,7 +59,7 @@ Template.exebalancesheetreport.onRendered(() => {
         $(".titleMonth2").html(currMonth2);
     }
     templateObject.initDate = () => {
-        Datehandler.initOneMonth();
+        Datehandler.initCurrentMonth();
     };
 
     templateObject.setDateAs = ( dateFrom = null ) => {
