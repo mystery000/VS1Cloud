f64991d3b5c25b93578548862d99fadae46bb866
mystery000 <lkostyrka17@gmail.com>
2023-01-18 16:26:07 +0200

    FIx: salesorders list

--- client/salesorder/salesorder_express_list_tables.js
+++ client/salesorder/salesorder_express_list_tables.js
@@ -70,7 +70,7 @@ Template.salesorderslist.onRendered(function() {
              for (let i = 0; i < data.ProcessLog.Obj.CustomLayout.length; i++) {
                if(data.ProcessLog.Obj.CustomLayout[i].TableName == listType){
                  reset_data = data.ProcessLog.Obj.CustomLayout[i].Columns;
-                 showCustomFieldDisplaySettings(reset_data);
+                 showCustomFieldDisplaySettings(Template.instance().reset_data.get());
                }
              }
            };
