a518bb9843acb9c45feaa13ca3c415a500c00467
mystery000 <lkostyrka17@gmail.com>
2023-01-26 00:09:06 -0800

    Fix commit

--- client/js/new_invoice.js
+++ client/js/new_invoice.js
@@ -8231,6 +8231,7 @@ Template.new_invoice.events({
     }
   },
   "click .btnRemove": async function (event) {
+    
     let templateObject = Template.instance();
     let taxcodeList = templateObject.taxraterecords.get();
     let utilityService = new UtilityService();
