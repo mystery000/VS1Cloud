780b85c72c4b902285417ee607430d1be28deb2b
Yurii <viper020107@gmail.com>
2023-02-08 03:51:39 +0900

    mobile page update

--- client/manufacture/mobile/main.js
+++ client/manufacture/mobile/main.js
@@ -2,25 +2,38 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './layout/header.html'
 import './main.html';
+import './container/startbreak.html';
 
 var html5QrcodeScannerProdModal = null;
 
 Template.mobileapp.onCreated(function() {
-    const templateObject = Template.instance();
-    templateObject.datatablerecords = new ReactiveVar([]);
-    templateObject.datatablerecords.set([{ID: 1, KeyValue: 0, Description: "hello"}]);
 })
-
-Template.mobileapp.fetchEmployeeList = () => {
-    getVS1Data('TEmployee').then(function (dataObject) {
-        console.log(dataObject)
-    });
-}
 
 Template.mobileapp.events({
     'click #btnOpentList': function(e, instance) {
         getVS1Data('TEmployee').then(function (dataObject) {
-            console.log(dataObject)
+            $(".mobile-left-employee-list").css('display', 'block');
+            let empdata = JSON.parse(dataObject[0].data);
+            let table = $("#tblEmployeeList").DataTable({
+                data: empdata.temployee,
+                paging: false,
+                searching: true,
+                dom: 't',
+                scrollY: document.getElementsByClassName('mobile-right-btn-containner')[0].clientHeight - 58 + 'px',
+                scrollCollapse: true,
+                autoWidth: true,
+                sScrollXInner: "100%",
+                columns: [
+                    { title: 'FirstName', mData: 'fields.FirstName' },
+                    { title: 'LastName', mData: 'fields.LastName' },
+                ]
+            })
+            $('#tblEmployeeList tbody').on('click', 'tr', function () {
+                var data = table.row(this).data();
+                $(".mobile-main-input").val(data.fields.EmployeeName)
+            });
+            $("#startBreakContainer").css('display', 'none');
+            $(".mobile-left-btn-containner").css('display', 'none');
         });
     },
     'click .mobile-btn-number': function(e, instance) {
@@ -53,17 +66,55 @@ Template.mobileapp.events({
         $("#btnStartJob").css('background', '#00AE00');
         $("#btnStartBreak").css('background', '#00AE00');
         $("#btnClockOut").css('background', '#C5000B');
+        $('#btnClockIn').prop('disabled', true);
+        $("#btnClockOut").removeAttr('disabled');
+        $("#btnStartBreak").removeAttr('disabled');
+        $("#btnStartJob").removeAttr('disabled');
         $(".mobile-header-status-text").text("Select Employee");
     },
+    'click #btnClockOut': function (e, instance) {
+        $('#btnClockOut').prop('disabled', true);
+        $("#btnClockOut").css('background', '#0084D1');
+        $("#btnClockIn").removeAttr('disabled');
+        $('#btnStartJob').prop('disabled', true);
+        $('#btnStartBreak').prop('disabled', true);
+        $("#btnClockIn").css('background', '#00AE00');
+        $("#btnStartJob").css('background', '#0084D1');
+        $("#btnStartBreak").css('background', '#0084D1');
+        $("#btnStopJob").css('background', '#0084D1');
+        $('#btnStopJob').prop('disabled', true);
+        $('#btnStopBreak').prop('disabled', true);
+    },
     'click #btnStartJob': function(e, instance) {
+        $('#btnStartJob').prop('disabled', true);
         $("#btnStartJob").css('background', '#999');
         $("#btnStopJob").css('background', '#C5000B');
+        $("#btnStopJob").removeAttr('disabled');
     },
     'click #btnStartBreak': function(e, instance) {
+        $("#startBreakContainer").css('display', 'block');
+        $('#btnStartBreak').prop('disabled', true);
         $("#btnStartBreak").css('background', '#999');
         $("#btnStopBreak").css('background', '#C5000B');
+        $("#btnStopBreak").removeAttr('disabled');
+        $(".mobile-left-btn-containner").css('display', 'none');
+        $(".mobile-left-employee-list").css('display', 'none');
+    },
+    'click #btnStopJob': function(e, instance) {
+        $('#btnStopJob').prop('disabled', true);
+        $("#btnStartJob").css('background', '#00AE00');
+        $("#btnStopJob").css('background', '#0084D1');
+        $("#btnStartJob").removeAttr('disabled');
+    },
+    'click #btnStopBreak': function(e, instance) {
+        $(".mobile-left-btn-containner").css('display', 'block');
+        $("#startBreakContainer").css('display', 'none');
+        $('#btnStopBreak').prop('disabled', true);
+        $("#btnStartBreak").css('background', '#00AE00');
+        $("#btnStopBreak").css('background', '#0084D1');
+        $("#btnStartBreak").removeAttr('disabled');
     },
     'click #mobileBtnEnter': function(e, instance) {
-        Template.mobileapp.fetchEmployeeList();
+        $("#frmOnHoldModal").modal();
     }
 });
