e5058086e0053d7cae9097b3ac8f6a5904e41b2d
Tinyiko Miracle Mabale <mabale@vs1cloud.com>
2023-02-28 16:43:14 +0200

    Tinyiko - EFT Options

--- client/lib/global/indexdbstorage.js
+++ client/lib/global/indexdbstorage.js
@@ -542,7 +542,13 @@ openDb = function (dbName) {
       db.createObjectStore("PrintDisplaySettings", { keyPath: "EmployeeEmail" });
       db.createObjectStore("TPNLLayout", { keyPath: "EmployeeEmail" });
       db.createObjectStore("ManufacturingSettings", { keyPath: "EmployeeEmail"});
-
+      db.createObjectStore("TDashboardExecData1", { keyPath: "EmployeeEmail"});
+      db.createObjectStore("TDashboardSalesData1", { keyPath: "EmployeeEmail"});
+      db.createObjectStore("TDashboardSalesData2", { keyPath: "EmployeeEmail"});
+      db.createObjectStore("TDashboardSalesData3", { keyPath: "EmployeeEmail"});
+      db.createObjectStore("TDashboardMyData1", { keyPath: "EmployeeEmail"});
+      db.createObjectStore("TDashboardMyData2", { keyPath: "EmployeeEmail"});
+      db.createObjectStore("TDashboardMyData3", { keyPath: "EmployeeEmail"});
       db.createObjectStore("BuildProfitabilityReport", { keyPath: "EmployeeEmail"});
       db.createObjectStore("ProductionWorksheetReport", { keyPath: "EmployeeEmail"});
       db.createObjectStore("WorkOrderReport", { keyPath: "EmployeeEmail"});
