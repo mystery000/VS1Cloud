56cc021103aeebc208d0c1b0b9b87917804a8d8a
mingli <whitespider0311@gmail.com>
2023-03-13 08:59:24 +0300

    Update Refunds_list and Uomlistpop by using the template

--- client/js/sidebar-service.js
+++ client/js/sidebar-service.js
@@ -3957,7 +3957,7 @@ export class SideBarService extends BaseService {
     return this.getList(this.ERPObjects.TRefundSale, options);
   }
 
-  getAllTRefundSaleListData(dateFrom,dateTo,ignoreDate,limitcount,limitfrom) {
+  getAllTRefundSaleListData(dateFrom,dateTo,ignoreDate,limitcount,limitfrom, deleteFilter) {
     let options = "";
 
     if (ignoreDate == true) {
@@ -3979,6 +3979,7 @@ export class SideBarService extends BaseService {
         LimitFrom: parseInt(limitfrom),
       };
     }
+    if(deleteFilter) options.Search = "";
     return this.getList(this.ERPObjects.TRefundSaleList, options);
   }
 
