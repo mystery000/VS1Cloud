eec8c228c160a08d9ac23c75c6a0388ea7ab37b4
martin <suhaa18@zoho.com>
2023-03-29 14:25:37 +0200

    fix getbanknamelist api

--- client/lib/global/bank-names.js
+++ client/lib/global/bank-names.js
@@ -3,8 +3,7 @@ export class BankNameService extends BaseService {
     getBankNameList() {
         let options = {
             PropertyList: 'BankName,BankCode',
-            select: "[Active]=true",
-            Region: localStorage.getItem('ERPLoggedCountry')
+            select: "[Active]=true and [Region]="+localStorage.getItem('ERPLoggedCountry')+"",            
         };
         return this.getList(this.ERPObjects.TBankCode, options);        
     }    
