e2493f3704e5083ac00bae61d72f4060483fef4a
Damien Rolling <damien.rolling@outlook.fr>
2023-04-03 12:00:36 +0200

    damien - fix tproductqty to tproductvs1

--- client/vs1_templates/internal_transaction_list/internal_transaction_list_with_switchbox.js
+++ client/vs1_templates/internal_transaction_list/internal_transaction_list_with_switchbox.js
@@ -400,11 +400,11 @@ Template.internal_transaction_list_with_switchbox.onRendered(function() {
     //Products Data
     templateObject.getProductsData = async function(deleteFilter = false) { //GET Data here from Web API or IndexDB
         var customerpage = 0;
-        getVS1Data('TProductQtyList').then(function(dataObject) {
+        getVS1Data('TProductVS1').then(function(dataObject) {
 
             if (dataObject.length == 0) {
-                sideBarService.getProductListVS1(initialBaseDataLoad, 0, deleteFilter).then(async function(data) {
-                    await addVS1Data('TProductQtyList', JSON.stringify(data));
+                sideBarService.getNewProductListVS1(initialBaseDataLoad, 0, deleteFilter).then(async function(data) {
+                    await addVS1Data('TProductVS1', JSON.stringify(data));
                     templateObject.displayProductsData(data); //Call this function to display data on the table
                 }).catch(function(err) {
 
@@ -414,8 +414,8 @@ Template.internal_transaction_list_with_switchbox.onRendered(function() {
                 templateObject.displayProductsData(data); //Call this function to display data on the table
             }
         }).catch(function(err) {
-            sideBarService.getProductListVS1(initialBaseDataLoad, 0, deleteFilter).then(async function(data) {
-                await addVS1Data('TProductQtyList', JSON.stringify(data));
+            sideBarService.getNewProductListVS1(initialBaseDataLoad, 0, deleteFilter).then(async function(data) {
+                await addVS1Data('TProductVS1', JSON.stringify(data));
                 templateObject.displayProductsData(data); //Call this function to display data on the table
             }).catch(function(err) {
 
@@ -437,30 +437,30 @@ Template.internal_transaction_list_with_switchbox.onRendered(function() {
             deleteFilter = false;
         };
         for (let i = 0; i < data.tproductqtylist.length; i++) {
-            if (data.tproductqtylist[i].Active == true) {
+            if (data.tproductvs1[i].Active == true) {
                 linestatus = "";
-            } else if (data.tproductqtylist[i].Active == false) {
+            } else if (data.tproductvs1[i].Active == false) {
                 linestatus = "In-Active";
             };
             chkBox = '<div class="custom-control custom-switch chkBox pointer chkServiceCard" style="width:15px;"><input name="pointer" class="custom-control-input chkBox pointer chkServiceCard" type="checkbox" id="productCheck-' + data.tproductqtylist[i].PARTSID +
-                '"><label class="custom-control-label chkBox pointer" for="productCheck-' + data.tproductqtylist[i].PARTSID +
+                '"><label class="custom-control-label chkBox pointer" for="productCheck-' + data.tproductvs1[i].ID +
                 '"></label></div>'; //switchbox
 
             costprice = utilityService.modifynegativeCurrencyFormat(
-                Math.floor(data.tproductqtylist[i].BuyQTY1 * 100) / 100); //Cost Price
+                Math.floor(data.tproductqtylist[i].BuyQty1Cost * 100) / 100); //Cost Price
             sellprice = utilityService.modifynegativeCurrencyFormat(
-                Math.floor(data.tproductqtylist[i].SellQTY1 * 100) / 100); //Sell Price
+                Math.floor(data.tproductqtylist[i].SellQty1Price * 100) / 100); //Sell Price
 
             var dataList = [
                 chkBox,
-                data.tproductqtylist[i].PARTSID || "",
+                data.tproductqtylist[i].ID || "",
                 data.tproductqtylist[i].ProductName || "",
                 data.tproductqtylist[i].SalesDescription || "",
                 data.tproductqtylist[i].BARCODE || "",
                 costprice,
                 sellprice,
-                data.tproductqtylist[i].InstockQty,
-                data.tproductqtylist[i].PurchaseTaxcode || "",
+                data.tproductqtylist[i].TotalQtyInStock,
+                data.tproductqtylist[i].TaxCodePurchase || "",
                 data.tproductqtylist[i].PRODUCTCODE || "",
                 data.tproductqtylist[i].Ex_Works || null,
                 linestatus,
@@ -2126,19 +2126,19 @@ Template.internal_transaction_list_with_switchbox.onRendered(function() {
         let fullAccountTypeName = "";
         let accBalance = "";
         let deleteFilter = false;
-        
+
         for (let i = 0; i < data.length; i++) {
             let lineData = data[i];
             let chkBoxId = "f-" + lineData.ID || lineData.Id;
             let chkBox = '<div class="custom-control custom-switch chkBox pointer chkServiceCard text-center" style="margin-right: -8px"><input name="pointer" class="custom-control-input chkBox pointer chkServiceCard" type="checkbox" id="' + chkBoxId + '" checked="true"><label class="custom-control-label chkBox pointer" for="' + chkBoxId + '"></label></div>'; //switchbox
-            let amount = utilityService.modifynegativeCurrencyFormat(Math.floor(lineData.Amount * 100) / 100);            
+            let amount = utilityService.modifynegativeCurrencyFormat(Math.floor(lineData.Amount * 100) / 100);
             var dataList = [
                 chkBox,
                 lineData.ID || lineData.Id || "",
                 `<input
                     class="form-control pointer sltEftTblAccountName es-input bg-white highlightSelect"
                     value="${lineData.AccountName || ""}"                    
-                />`,                 
+                />`,
                 lineData.BSB || "___-___",
                 lineData.CreditDebitAccountNumber || "",
                 `<input
@@ -2219,7 +2219,7 @@ Template.internal_transaction_list_with_switchbox.onRendered(function() {
                 },
                 language: { search: "", searchPlaceholder: "Search List..." },
                 // searching: true,
-                "fnInitComplete": function(oSettings) {                    
+                "fnInitComplete": function(oSettings) {
                     $("<button class='btn btn-primary' data-dismiss='modal' data-toggle='modal' data-target='#newDepartmentModal' type='button' style='padding: 4px 10px; font-size: 16px; margin-left: 12px !important;'><i class='fas fa-plus'></i></button>").insertAfter('#' + currenttablename + '_filter');
                     if (data?.Params?.Search?.replace(/\s/g, "") == "") {
                         $("<button class='btn btn-danger btnHideDeleted' type='button' id='btnHideDeleted' style='padding: 4px 10px; font-size: 16px; margin-left: 14px !important;'><i class='far fa-check-circle' style='margin-right: 5px'></i>Hide In-Active</button>").insertAfter('#' + currenttablename + '_filter');
