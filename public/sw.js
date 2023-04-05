6e0854466a5a8a2d5cd3d3a7a892e21c9d800319
trueerp_cloud <devsa@trueerp.com>
2023-04-05 10:58:51 +0200

    Rashed Push Fix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const HTMLToCache = '/';
-const version = 'MSW V0.252';
+const version = 'MSW V0.255';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(version).then((cache) => {
